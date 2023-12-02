c128136db112f18f987c0c01ea0e7df93f7dedd9
Raushan0303 <110516637+Raushan0303@users.noreply.github.com>
2023-12-02 22:43:11 +0530

    chnage happend

--- App.js
+++ App.js
@@ -24,6 +24,7 @@ const Title = () => (
   className="logo"
   alt="logo"
    src="https://lh3.googleusercontent.com/p/AF1QipMKTUs5jo3Hoihm-GazeAIg7Pn5Gmr07GQD5o6W=w1080-h608-p-no-v0"></img>
+    
    </a>
 );
   
@@ -32,8 +33,9 @@ const Title = () => (
     return (
       <div className="header">
         <Title />
-        
+       
         <div className="nav-item">
+       
           <ul>
             <li>Home</li>
             <li>About</li>
@@ -90,6 +92,14 @@ const Title = () => (
 ];
 //props is like object of resturant
 //instead of props pass as parameter we can pass {resturant} -> this is like destructuring the object
+const Searchbar = () =>{
+  return (
+    <a className="searchbar">
+      <input type="text" placeholder="Search.."></input>
+      <button>Search</button>
+    </a>
+  )
+}
 
   const ResturantCard = (
     {
@@ -133,6 +143,7 @@ const Title = () => (
     return (
       <>
         <HeaderComponent />
+        <Searchbar />
         <Body />
         <Footer />
       </>
